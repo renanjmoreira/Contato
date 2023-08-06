0788b42ebbd81e9ebbdb9283f836dbd107b9c760
Renan Moreira <renanj-moreira@outlook.com>
2023-08-06 19:25:11 -0300

    chore: configura contexto, mediatr e validators

--- ContatoAPI/Program.cs
+++ ContatoAPI/Program.cs
@@ -1,5 +1,22 @@
+using ContatoAPI.Application.Commands;
+using ContatoAPI.Infrastructure;
+using FluentValidation.AspNetCore;
+using Microsoft.EntityFrameworkCore;
+using System.Reflection;
+
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Services.AddDbContext<ContextoBd>(options => options.UseInMemoryDatabase("InMemoryDb"));
+
+builder.Services.AddMediatR(cfg => cfg.RegisterServicesFromAssemblies(Assembly.GetExecutingAssembly()));
+
+builder.Services.AddFluentValidation(fv => {
+    fv.RegisterValidatorsFromAssemblyContaining<AdicionarPessoaCommandValidator>();
+    fv.RegisterValidatorsFromAssemblyContaining<ContatoAdicionarPessoaValidator>();
+
+    fv.RegisterValidatorsFromAssemblyContaining<EditarPessoaCommandValidator>();
+    fv.RegisterValidatorsFromAssemblyContaining<ContatoEditarPessoaValidator>();
+});
 
 
 builder.Services.AddControllers();
