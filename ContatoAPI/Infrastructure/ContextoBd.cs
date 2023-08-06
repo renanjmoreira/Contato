4ee5fb39297e3ca9784a9f71e79be063b4c970bf
Renan Moreira <renanj-moreira@outlook.com>
2023-08-06 18:59:25 -0300

    feat: adiciona contexto em memoria

---
+++ ContatoAPI/Infrastructure/ContextoBd.cs
@@ -0,0 +1,15 @@
+﻿using ContatoAPI.Domain.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ContatoAPI.Infrastructure
+{
+    public class ContextoBd : DbContext
+    {
+        public ContextoBd(DbContextOptions<ContextoBd> options) : base(options)
+        {
+        }
+
+        public DbSet<Pessoa> Pessoas { get; set; }
+        public DbSet<Contato> PessoaContatos { get; set; }
+    }
+}
